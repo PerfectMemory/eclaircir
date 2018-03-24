4cc6732e580ecd5940a4d993897ddaa91f0d8a46
Karim Bogtob <kabogtob@gmail.com>
2018-03-24 18:26:13 +0100

    Setup Clarifier Configuration

--- lib/clarifier.rb
+++ lib/clarifier.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 module Clarifier
+  class << self
+    def configure
+      yield configuration
+    end
 
+    def configuration
+      @configuration ||= Configuration.new
+    end
+  end
 end
 
 require_relative 'clarifier/version'
+require_relative 'clarifier/configuration'
