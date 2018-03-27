11f19c03413c8d3d9a3507420f91da7c6462ab0f
Karim Bogtob <karim.bogtob@perfect-memory.com>
2018-03-27 10:45:57 +0200

    Add Output model

---
+++ lib/clarifier/api_models/output.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative 'base_model'
+
+module Clarifier
+  class Output < BaseModel
+    attribute :id, String
+    attribute :status, Status
+    attribute :created_at, Attributes::NullableDateTime
+    attribute :model, Model
+    attribute :input, Input
+    attribute :data, Data
+  end
+end
