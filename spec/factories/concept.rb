6f2d627718c27d88d685427f38a83ed817c71806
Karim Bogtob <karim.bogtob@perfect-memory.com>
2018-03-31 01:45:23 +0200

    Use ingredient instead of dish to support old version of faker

--- spec/factories/concept.rb
+++ spec/factories/concept.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :concept, class: Clarifier::Concept do
     id { Faker::Base.regexify(%r/ai_[A-z0-9]{8}/) }
-    name { Faker::Food.dish }
+    name { Faker::Food.ingredient }
     value { rand(0.4..1.0).round(7) }
     initialize_with { Clarifier::Concept.new(attributes) }
   end
--- spec/factories/model.rb
+++ spec/factories/model.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :model, class: Clarifier::Model do
     id { Faker::Base.regexify(%r/[a-f0-9]{32}/) }
-    name { Faker::Food.dish }
+    name { Faker::Food.ingredient }
     created_at { DateTime.now }
     app_id nil
     initialize_with { Clarifier::Model.new(attributes) }
