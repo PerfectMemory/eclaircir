d70dfe1f546a1f944b0ddea04e9e6274ef8f925e
Karim Bogtob <karim.bogtob@perfect-memory.com>
2018-03-31 00:26:04 +0200

    Add status validation management

--- lib/clarifier/api_models/status.rb
+++ lib/clarifier/api_models/status.rb
@@ -6,5 +6,41 @@ module Clarifier
   class Status < BaseModel
     attribute :code, Integer
     attribute :description, String
+
+    def success?
+      code == SUCCESS
+    end
+
+    def validate!
+      unless success?
+        error_class = ERRORS_MAPPINGS.fetch(code, APIError)
+        raise error_class, description
+      end
+
+      true
+    end
+
+    # statuses
+    SUCCESS = 10_000
+
+    FAILURE = 10_020
+
+    INVALID_AUTH_TOKEN = 11_001
+    API_KEY_NOT_FOUND = 11_009
+
+    BAD_REQUEST_FORMAT = 11_100
+    INVALID_REQUEST = 11_102
+
+    DUPLICATE_URL = 30_100
+    IMAGE_DECODING_FAILED = 30_300
+
+    ERRORS_MAPPINGS = {
+      INVALID_AUTH_TOKEN    => InvalidAuthTokenError,
+      API_KEY_NOT_FOUND     => ApiKeyNotFoundError,
+      BAD_REQUEST_FORMAT    => BadRequestFormatError,
+      INVALID_REQUEST       => InvalidRequestError,
+      DUPLICATE_URL         => DuplicateURLError,
+      IMAGE_DECODING_FAILED => ImageDecodingError,
+    }.freeze
   end
 end
--- spec/clarifier/api_models/status_spec.rb
+++ spec/clarifier/api_models/status_spec.rb
@@ -7,11 +7,61 @@ describe Clarifier::Status do
 
   let(:params) do
     {
-      'code' => 10_000,
+      'code' => code,
       'description' => 'Ok',
     }
   end
 
+  let(:code) do
+    10_000
+  end
+
   its(:code) { is_expected.to eq 10_000 }
   its(:description) { is_expected.to eq 'Ok' }
+
+  describe '#success?' do
+    context 'when the code is Status::SUCCESS' do
+      its(:success?) { is_expected.to be true }
+    end
+
+    context 'when the code is not Status::SUCCESS' do
+      let(:code) do
+        10_020
+      end
+
+      its(:success?) { is_expected.to be false }
+    end
+  end
+
+  describe '#validate!' do
+    context 'when the code is Status::SUCCESS' do
+      it 'returns true' do
+        expect(subject.validate!).to be true
+      end
+    end
+
+    context 'when the code has a specific error' do
+      let(:code) do
+        30_300
+      end
+
+      it 'raises the specific error with the description' do
+        expect do
+          subject.validate!
+        end.to raise_error(Clarifier::ImageDecodingError, /Ok/)
+      end
+    end
+
+    context 'when the code is not managed' do
+      let(:code) do
+        99_999
+      end
+
+      it 'raises an APIError' do
+        expect do
+          subject.validate!
+        end.to raise_error(Clarifier::APIError, /Ok/)
+      end
+    end
+  end
 end
