c651e6e677ba86f2bf70e6f89a8e6e3041c5ae1c
Karim Bogtob <karim.bogtob@perfect-memory.com>
2018-03-31 16:48:33 +0200

    Make Medias have a base64 attribute

--- lib/eclaircir/api_models/media.rb
+++ lib/eclaircir/api_models/media.rb
@@ -5,5 +5,10 @@ require_relative 'base_model'
 module Eclaircir
   class Media < BaseModel
     attribute :url, String
+    attribute :base64, String
+
+    def to_api_hash
+      super.compact
+    end
   end
 end
